a4a7010c5d7e54ede40577c3c4ab67024acbbc80
Kerem Gözaydin <gozaydinkerem9@gmail.com>
2024-03-18 21:49:17 +0300

    fotolar eklendi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class='h-full w-full flex flex-col px-12 pb-12 pt-12 items-center justify-center gap-12 text-center'>
 	<div class='w-1/2 m-auto card card-hover flex flex-col items-center justify-center'>
-		<header class='w-full aspect-[16/9] bg-surface-100 text-surface-900 grid place-items-center'>foto</header>
+		<img alt='integra' src='/integra0.png' class='w-full aspect-[16/9] bg-surface-100 text-surface-900 grid place-items-center'/>
 		<div class='card-footer p-4'>
 			<h2>INTEGRA 3646</h2>
 			<p>INTEGRA 3646'nın  websitesine hoş geldiniz!</p>
--- src/routes/basarilarimiz/+page.svelte
+++ src/routes/basarilarimiz/+page.svelte
@@ -1,10 +1,10 @@
 <div class='pt-4 pb-8 w-full h-full flex flex-row flex-wrap justify-center items-center'>
     <h1>Başarılarımız!</h1>
     <div class='h-full w-full flex flex-col justify-center items-center'>
-        <div class='card flex flex-col gap-4 justify-center w-3/4 sm:w-2/3 p-6 rounded-md'>
+        <div class='card flex flex-col gap-4 justify-center w-3/4 sm:w-1/2 p-6 rounded-md'>
             <div class='w-full'>
-                <div class='w-full grid place-items-center aspect-[16/9] bg-surface-50 text-black mb-2'>foto</div>
-                <div class='text-center italic text-sm text-surface-300'>Integra 3646 ilk foto</div>
+                <img alt='integra' src='/integra3.jpeg' class='w-full grid place-items-center aspect-[12/9] bg-surface-50 text-black mb-2'/>
+                <div class='text-center italic text-sm text-surface-300'>Integra 3646 takım fotoğrafı!</div>
             </div>
             <h2 class='text-center'>2011</h2>
             <ul class='list'>
@@ -21,8 +21,8 @@
                 </li>
             </ul>
             <div class='w-full'>
-                <div class='w-full grid place-items-center  aspect-[16/9] bg-surface-50 text-black mb-2'>foto</div>
-                <div class='text-center italic text-sm text-surface-300'>Okul ile foto</div>
+                <img alt='integra' src='/integra6.jpeg' class='w-full grid place-items-center  aspect-[16/9] bg-surface-50 text-black mb-2'/>
+                <div class='text-center italic text-sm text-surface-300'>Integra Cem Seymen ile!</div>
             </div>
             <h2 class='text-center'>2016</h2>
             <ul class='list'>
@@ -43,8 +43,8 @@
                 </li>
             </ul>
             <div class='w-full'>
-                <div class='w-full grid place-items-center aspect-[16/9] bg-surface-50 text-black mb-2'>foto</div>
-                <div class='text-center italic text-sm text-surface-300'>Okul ile foto</div>
+                <img alt='integra' src='/integra2.jpeg' class='w-full grid place-items-center aspect-[16/9] bg-surface-50 text-black mb-2'/>
+                <div class='text-center italic text-sm text-surface-300'>2019 Integra takımı!</div>
             </div>
             <h2 class='text-center'>2018</h2>
             <ul class='list'>
@@ -77,8 +77,8 @@
                 </li>
             </ul>
             <div class='w-full'>
-                <div class='w-full grid place-items-center aspect-[16/9] bg-surface-50 text-black mb-2'>foto</div>
-                <div class='text-center italic text-sm text-surface-300'>Okul ile foto</div>
+                <img alt='integra' src='/integra4.jpeg' class='w-full grid place-items-center  aspect-[4/3] bg-surface-50 text-black mb-2'/>
+                <div class='text-center italic text-sm text-surface-300'>Integra Teknofestte!</div>
             </div>
             <h2 class='text-center'>2020</h2>
             <ul>
